dbe4affa357c368258b86abb0d1f944c681c2e8d
tanvir <tanvirhridoy1011@gmail.com>
2022-09-03 01:04:25 +0600

    catagories added dynamically

--- js/news.js
+++ js/news.js
@@ -1,1 +1,18 @@
-console.log('its connected')
+const loadCatagories=async()=>{
+    const url=`https://openapi.programming-hero.com/api/news/categories`
+    const res=await fetch(url);
+    const data=await res.json();
+    displayCatagories(data.data.news_category)
+}
+const displayCatagories=catagories=>{
+    const catagoriesContainer=document.getElementById('catagories-container');
+    catagories.forEach(catagori => {
+        const catagoriDiv=document.createElement('div');
+        catagoriDiv.innerHTML=`<div class="p-3 border bg-light">${catagori. category_name}</div>`;
+        // console.log(catagori. category_name)
+        catagoriesContainer.appendChild(catagoriDiv);
+    });
+   
+    // console.log(catagories)
+}
+loadCatagories()
